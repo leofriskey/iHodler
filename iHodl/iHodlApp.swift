e22db96ab34e7f2d4fbeb24308a37fb1488024c3
Leo Friskey <squire_conceit0e@icloud.com>
2022-07-25 23:34:05 +0500

    Initial Commit

---
+++ iHodl/iHodlApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHodlApp.swift
+//  iHodl
+//
+//  Created by Leo Friskey on 25.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct iHodlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
